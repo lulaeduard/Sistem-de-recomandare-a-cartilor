e68c26bba9646e3880917f47ef3f9d043672e6e9
lulaeduard <lula.eduard.k7x@student.ucv.ro>
2024-12-07 16:37:33 +0200

    Carte.cpp

---
+++ Carte.cpp
@@ -0,0 +1,20 @@
+#include "Carte.h"
+
+Carte::Carte(const std::string& titlu, const std::string& autor, const std::string& domeniu, int nivel)
+    : titlu(titlu), autor(autor), domeniu(domeniu), nivelDificultate(nivel) {}
+
+std::string Carte::getTitlu() const {
+    return titlu;
+}
+
+std::string Carte::getDomeniu() const {
+    return domeniu;
+}
+
+int Carte::getNivelDificultate() const {
+    return nivelDificultate;
+}
+
+std::string Carte::afisare() const {
+    return "Titlu: " + titlu + ", Autor: " + autor + ", Domeniu: " + domeniu + ", Nivel: " + std::to_string(nivelDificultate);
+}
