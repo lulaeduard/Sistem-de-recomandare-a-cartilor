b3eb8ac86934e31125d99880050c996b16e530f5
lulaeduard <lula.eduard.k7x@student.ucv.ro>
2024-12-07 16:35:32 +0200

    Utilizator.cpp

---
+++ Utilizator.cpp
@@ -0,0 +1,12 @@
+#include "Utilizator.h"
+
+Utilizator::Utilizator(const std::string& username, const std::string& parola)
+    : username(username), parola(parola) {}
+
+std::string Utilizator::getUsername() const {
+    return username;
+}
+
+bool Utilizator::verificaParola(const std::string& parola) const {
+    return this->parola == parola;
+}
