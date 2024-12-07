e22fdf707e1e795dfe9e3b285fe2a897ac716d35
lulaeduard <lula.eduard.k7x@student.ucv.ro>
2024-12-07 16:37:07 +0200

    Carte.h

---
+++ Carte.h
@@ -0,0 +1,22 @@
+#ifndef CARTE_H
+#define CARTE_H
+
+#include <string>
+
+class Carte {
+private:
+    std::string titlu;
+    std::string autor;
+    std::string domeniu;
+    int nivelDificultate;
+
+public:
+    Carte(const std::string& titlu, const std::string& autor, const std::string& domeniu, int nivel);
+
+    std::string getTitlu() const;
+    std::string getDomeniu() const;
+    int getNivelDificultate() const;
+    std::string afisare() const;
+};
+
+#endif
