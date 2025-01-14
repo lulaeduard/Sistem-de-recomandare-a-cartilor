b41457f2c95e062446a95772eb1339d51f8a8498
lulaeduard <lula.eduard.k7x@student.ucv.ro>
2025-01-14 22:13:09 +0200

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -2,10 +2,17 @@
 #define ADMIN_H
 
 #include "Utilizator.h"
+#include "Carte.h"
+#include <vector>
+#include <string>
 
 class Admin : public Utilizator {
 public:
     Admin(const std::string& username, const std::string& parola);
+    void adaugaCarte(std::vector<Carte>& carti, const Carte& carte) const;
+    void stergeCarte(std::vector<Carte>& carti, const std::string& titlu) const;
+    void vizualizeazaCarti(const std::vector<Carte>& carti) const;
+    void vizualizeazaIstoric(const std::vector<std::pair<std::string, std::vector<std::string>>>& istoric) const;
 };
 
 #endif
